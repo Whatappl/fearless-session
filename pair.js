74117c88a71adf87c31151f19d9fce404f12881d
Fearless md <167037126+Whatappl@users.noreply.github.com>
2025-03-08 22:31:18 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -70,7 +70,7 @@ router.get('/', async (req, res) => {
 Please Follow My Support Channel
 Wanna talk to me?🫟 +27 68 083 7873🫟
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-3099 FEARLESS TECH*
+©*2024-3099 JAY TECH*
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
